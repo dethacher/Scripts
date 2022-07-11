6480d76e3972907f12a4b034bada630a2a22a5e7
dethacher <90139780+dethacher@users.noreply.github.com>
2022-07-11 13:24:19 -0400

    Update transactions.groovy

--- transactions.groovy
+++ transactions.groovy
@@ -6,7 +6,7 @@ println "\n\nScript Output"
 def file = new File("transactions " + new Date().toTimestamp().toString() + ".csv")
 file.text = ''
 
-new transaction().transaction(2021).each {
+new transaction().transaction().each {
 	if (it.Action != "Lineup" && it.Action != "LM")
 		file << it.Date + "," + it.Week + "," + it.Action + "," + it.Player_Name + " " + it.Player_Position + "," + it.From + "," + it.To + "," + it.Owner + "," + it.Player_Position + "," + it.Cost + "," + it.Player_Team + "\n"
 }
